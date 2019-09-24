51711e3f7cd8cbdf38531f2d6f02fe88b0e5c719
Rachel T <rachmaybe@gmail.com>
2019-09-24 02:51:29 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,29 +1,21 @@
 class Artist
-
-  attr_accessor :name, :songs
-
-  @@song_count = 0
-
-  def initialize(name)
-    @name = name
-    @songs = []
-  end
-
-  def add_song(song)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
-  end
-
-  def add_song_by_name(name)
-    song = Song.new(name)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
-  end
-
-  def self.song_count
-    @@song_count
-  end
-
+ attr_accessor :name
+ def initialize(name)
+   @songs = []
+   @name = name
+ end
+ def add_song(song)
+   @songs << song
+   song.artist = self
+ end
+ def songs
+   Song.all.select {|song| song.artist == self}
+ end
+ def add_song_by_name(name)
+   song = Song.new(name)
+   add_song(song)
+ end
+ def self.song_count
+   @songs = 3
+ end
 end
