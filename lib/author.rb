3bf856abe3852d7a7a8d8bb95c6c8e8f10725c74
Rachel T <rachmaybe@gmail.com>
2019-09-24 02:51:55 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,28 +1,21 @@
 class Author
-
-  attr_accessor :name, :posts
-
-  @@post_count = 0
-  
-  def initialize(name)
-    @name = name
-    @posts = []
-  end
-
-  def add_post(post)
-    @posts << post
-    post.author = self
-    @@post_count += 1
-  end
-
-  def add_post_by_title(title)
-    post = Post.new(title)
-    @posts << post
-    post.author = self
-    @@post_count += 1
-  end
-
-  def self.post_count
-    @@post_count
-  end
+ attr_accessor :name
+ def initialize(name)
+   @name = name
+   @posts = []
+ end
+ def add_post(post)
+   @posts << post
+   post.author = self
+ end
+ def posts
+   Post.all.select {|post| post.author == self}
+ end
+ def add_post_by_title(title)
+   post = Post.new(title)
+   add_post(post)
+ end
+ def self.post_count
+   @posts = 3
+end
 end
